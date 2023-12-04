0022e6ae68869eb46188e036b3ee379cc1a52968
mislavzanic <mislavzanic3@gmail.com>
2023-12-04 07:24:53 +0100

    2023 04

---
+++ y2023/d4.py
@@ -0,0 +1,20 @@
+from collections import Counter
+from aoc_util.advent import Input
+
+day = Input(day=4, year=2023)
+
+pt1 = 0
+pt2 = Counter()
+for i, line in enumerate(day.lines()):
+    pt2[i+1] += 1
+    nums = [
+        [x for x in filter(lambda x: x!='', line.split(" | ")[0].split(": ")[1].split(" "))],
+        [x for x in filter(lambda x: x != '', line.split(" | ")[1].split(" "))]
+    ]
+    winning = [x for x in filter(lambda x: x in nums[0], nums[1])]
+    if len(winning) > 0:
+        pt1 += pow(2, len(winning) - 1)
+    for j, _ in enumerate(winning):
+        pt2[i+j+2] += pt2[i+1]
+
+print(pt1, sum(pt2.values()))
